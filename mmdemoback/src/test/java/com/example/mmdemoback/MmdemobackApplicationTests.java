4e5236166356ba8294426d09ee2b2c037c185740
Sungho, Kim <john_sungho@naver.com>
2025-06-28 22:33:12 +0900

    Init: Backend

---
+++ mmdemoback/src/test/java/com/example/mmdemoback/MmdemobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mmdemoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmdemobackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
