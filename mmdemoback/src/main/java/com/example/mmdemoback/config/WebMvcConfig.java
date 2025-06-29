414a5d26f138e9adf96a27cc8330ca002b739ed0
Sungho, Kim <john_sungho@naver.com>
2025-06-30 07:12:23 +0900

    fix: 의존성 설정

--- mmdemoback/src/main/java/com/example/mmdemoback/config/WebMvcConfig.java
+++ mmdemoback/src/main/java/com/example/mmdemoback/config/WebMvcConfig.java
@@ -1,3 +1,10 @@
+package com.example.mmdemoback.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
