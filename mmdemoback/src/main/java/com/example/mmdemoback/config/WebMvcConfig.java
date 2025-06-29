6a3476a287420eb876c0ba42a75bd5f52472ca21
Sungho, Kim <john_sungho@naver.com>
2025-06-30 07:10:11 +0900

    feat: cors 문제 수정

---
+++ mmdemoback/src/main/java/com/example/mmdemoback/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final long MAX_AGE_SECS = 3600;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE",
+                        "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
+    }
+}
